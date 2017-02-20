89b3911dcd203fa913da5f178698b398c627f049
Osman Kale <osman.kale@protonmail.com>
2017-02-20 07:16:43 +0000

    latest commit

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
-
- $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(getQuote) {
-      $('.quote-text').append('<p class="text-center" id="text">'+getQuote.quote+'</p>')
+ function myQuote(){
+   $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(getQuote) {
+      $('.quote-text').empty().append('<p class="text-center" id="text">'+getQuote.quote+'</p>')
  
-      $('#authorName').append('<p class="text-center" id="text">'+getQuote.author+'</p>')
+      $('#authorName').empty().append('<p class="text-center" id="text">'+getQuote.author+'</p>')
       
       window.latestQuote = getQuote.quote;
       window.latestAuthor = getQuote.author;
  });
-  
- $('#newQuote').on('click',function() {
-  $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(getQuote) {
-      $('.quote-text').empty().append('<p class="text-center" id="text">'+getQuote.quote+'</p>')
+ }
  
-      $('#authorName').empty().append('<p class="text-center" id="text">'+getQuote.author+'</p>')
+ myQuote();
+ 
+ $('#newQuote').on('click',function() {
+   myQuote();
  });
- });
+ 
+
  
  $('#tweet').on('click', function(){
       var url = "http://twitter.com/intent/tweet?text=\"" + window.latestQuote + "\" - " + window.latestAuthor;
       $(this).attr("href", url);
- })
- 
- 
+ });
  
  });
